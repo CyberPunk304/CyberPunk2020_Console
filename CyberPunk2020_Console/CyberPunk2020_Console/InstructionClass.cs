f417c558cadfc72572df29d35516924e65857d79
DESKTOP-M87NU82\dante <kamil.dobrogost@gmail.com>
2018-08-31 06:58:03 +0200

    Add more instructions in InstructionClass

--- CyberPunk2020_Console/CyberPunk2020_Console/InstructionClass.cs
+++ CyberPunk2020_Console/CyberPunk2020_Console/InstructionClass.cs
@@ -47,39 +47,84 @@ namespace CyberPunk2020_Console
                 Console.WriteLine("Ta postać do trafienia z obydwu broni otrzymuję modyfikator -3");
             }
 
-            Console.Write("6.Rzuć 1K10 aby ustalić miejsce trafienia. Czy postać chce strzelić w konkretne miejsce na ciele przeciwnika T/N?");
+            Console.WriteLine("6. Czy postać strzela z broni automatycznej(np. Karabin, Uzi, Pistolet maszynowy) T/N?");
+            answer = Console.ReadLine();
+            if (answer == "T" || answer == "t")
+            {
+                Console.WriteLine("Postać strzelająca z broni automatycznej ma możliwość wyboru strzelania za pomocą:");
+                Console.WriteLine("\t- Pełna seria: Brak możliwości użycia celownika i celowania.");
+                Console.WriteLine("\t\t- Podziel maksymalną szybkostrzelność broni przez ilość celów do których zamierzasz strzelać");
+                Console.WriteLine("\t\t- Każde 10 pocisków przypadające na cel na bezpośredni,bliski dystans dodaje +1 do ataku");
+                Console.WriteLine("\t\t- Każde 10 pocisków przypadające na cel na średni,daleki,ekstremalny dystans odejmuję -1 do ataku");
+                Console.WriteLine("\t\t- Dla każdego celu oddzielnie przeprowadzić test czy został trafiony (Punkt 8)");
+                Console.WriteLine("\t\t- Na każdy punkt powyżej testu ataku z broni dystansowej (Punkt 8) pocisk trafia przeciwnika i należy osobno za nie policzyć obrażenia.");
+                Console.WriteLine("\t\t- np. test ma trudność 14 wynik testu ataku z broni dystansowej wyniósł 16 czyli tego przeciwnika trafiają 2 kule. Dla każdej kuli liczymy osobno obrażenia.");
+                //TODO: Opisać możliwość strzelania serią 3 pociskową.
+            }
+
+
+            Console.Write("7.Rzuć 1K10 aby ustalić miejsce trafienia. Czy postać chce strzelić w konkretne miejsce na ciele przeciwnika T/N?");
             answer = Console.ReadLine();
             if (answer == "T" || answer == "t")
             {
                 Console.WriteLine("Jeśli postać chce strzelić w konkretne miejsce w ciele przeciwnika dostaje modyfikator -4 do trafienia");
             }
 
-            Console.Write("7.Czy postać atakuję bronią dystansową T/N?");
+            Console.Write("8.Czy postać atakuję bronią dystansową T/N?");
             answer = Console.ReadLine();
+            if (answer == "T" || answer == "t")
+            {
+                Console.WriteLine("Należy rzucić 1K10 + REFLEKS + umiejętność posługiwania się bronią + modyfikatory");
+                Console.WriteLine("Jeśli na kostce wypadnie 10 rzuć nią jeszcze raz i wynik dodaj do poprzedniego");
+                Console.WriteLine("Jeśli na kostce wypadnie 1 strzał jest nie udany. Rzuć ponownie 1k10 i odczytaj rezultat");
+
+                Console.WriteLine("Czy rzut zakończył się krytyczną porażką T/N?");
+                answer = Console.ReadLine();
+                if (answer == "T" || answer == "t")
+                {
+                    Console.WriteLine("Zależnie od wyniku rzutu 1k10 wydarza się:");
+                    Console.WriteLine("\t1-4\tTym razem to nie pech po prostu zwaliłeś");
+                    Console.WriteLine("\t5\tUpuściłeś broń");
+                    Console.WriteLine("\t6\tBroń wypaliła i uderzyła w coś bez szwanku");
+                    Console.WriteLine("\t7\tBroń zacieła się lub utknęła w ziemi. Aby ją odblokować poświęć jedną turę");
+                    Console.WriteLine("\t8\tUdało Ci się zranić samego siebie. Wylosuj lokację i zadaj obrażenia jak przy odległości bezpośredniej");
+                    Console.WriteLine("\t9-10\tUdało Ci się zranić członka drużyny. Wylosuj lokację trafienia i zadaj jej obrażenia.");
+                }
+
+                Console.WriteLine("Kontynuować? T/N?");
+                answer = Console.ReadLine();
+                if (answer == "T" || answer == "t")
+                {
+                    Console.WriteLine("Maksymalne zasięgi broni:");
+                    Console.WriteLine("\t-Pistolet\t\t50m");
+                    Console.WriteLine("\t-Pistolet Maszynowy\t150m");
+                    Console.WriteLine("\t-Śrutówki\t\t50m");
+                    Console.WriteLine("\t-Karabiny\t\t400m");
+                    Console.WriteLine("\t-Rzut czymś w kogoś\t10 x Budowa ciała postaci wynik jest podawany w metrach");
+                    Console.WriteLine("Odległości od przeciwnika dzielimy na:");
+                    Console.WriteLine("\t-Bezpośrednia:\t\tBroń znajduję się bardzo blisko od celu lub jest z nim w fizycznym kontakcie.");
+                    Console.WriteLine("\t-Bliska:\t\tBroń atakuję z 1/4 podanego zasięgu");
+                    Console.WriteLine("\t-Średnia:\t\tBroń atakuję z 1/2 podanego zasięgu");
+                    Console.WriteLine("\t-Daleki:\t\tBroń atakuję z zasięgu podanego w jej opisie");
+                    Console.WriteLine("\t-Ekstremalna:\t\tBroń atakuję z odległości dwa razy większej niż podany zasięg");
+                    Console.WriteLine("Zależnie od odległości z jakiej postać strzela wynik musi być większy niż:");
+                    Console.WriteLine("\t-Bezpośredna:\t\t10");
+                    Console.WriteLine("\t-Bliska:\t\t14");
+                    Console.WriteLine("\t-Średnia:\t\t20");
+                    Console.WriteLine("\t-Daleki:\t\t25");
+                    Console.WriteLine("\t-Ekstremalna:\t\t30");
+                    Console.WriteLine("Jeśli wynik rzutu jest większy niż trudność testu postać trafia przeciwnika");
+                }
+            }
+
+            Console.Write("9.Czy postać atakuję bronią białą T/N?");
             if (answer == "T" || answer == "t")
             {
                 Console.WriteLine("Należy rzucić 1K10 + REFLEKS + umiejętność posługiwania się bronią + modyfikatory");
                 Console.WriteLine("Maksymalne zasięgi broni:");
                 Console.WriteLine("\t-Pistolet\t\t50m");
-                Console.WriteLine("\t-Pistolet Maszynowy\t150m");
-                Console.WriteLine("\t-Śrutówki\t\t50m");
-                Console.WriteLine("\t-Karabiny\t\t400m");
-                Console.WriteLine("\t-Rzut czymś w kogoś\t10 x Budowa ciała postaci wynik jest podawany w metrach");
-                Console.WriteLine("Odległości od przeciwnika dzielimy na:");
-                Console.WriteLine("\t-Bezpośredna:\t\tBroń znajduję się bardzo blisko od celu lub jest z nim w fizycznym kontakcie.");
-                Console.WriteLine("\t-Bliska:\t\tBroń atakuję z 1/4 podanego zasięgu");
-                Console.WriteLine("\t-Średnia:\t\tBroń atakuję z 1/2 podanego zasięgu");
-                Console.WriteLine("\t-Daleki:\t\tBroń atakuję z zasięgu podanego w jej opisie");
-                Console.WriteLine("\t-Ekstremalna:\t\tBroń atakuję z odległości dwa razy większej niż podany zasięg");
-                Console.WriteLine("Zależnie od odległości z jakiej postać strzela wynik musi być większy niż:");
-                Console.WriteLine("\t-Bezpośredna:\t\t10");
-                Console.WriteLine("\t-Bliska:\t\t14");
-                Console.WriteLine("\t-Średnia:\t\t20");
-                Console.WriteLine("\t-Daleki:\t\t25");
-                Console.WriteLine("\t-Ekstremalna:\t\t30");
-                Console.WriteLine("Jeśli wynik rzutu jest większy niż trudność testu postać trafia przeciwnika");
             }
-            Console.Write("8.Czy postać atakuję bronią białą T/N?");
+
 
             Console.Write("X.Czy pworócić do menu? Odpowiedź przecząca rozpocznie sekwencje od początku T/N?");
             //if (answer == "T" || answer == "t")
